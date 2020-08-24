959e127255a6aa0a7c5260144028e9ce86758907
Mauro Bonaldo <mbonaldo@gmail.com>
2020-08-24 13:32:08 +0200

    Try routing

--- src/ComingSoonServiceProvider.php
+++ src/ComingSoonServiceProvider.php
@@ -28,7 +28,7 @@ class ComingSoonServiceProvider extends ServiceProvider
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'mbonaldo');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'mbonaldo');
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadRoutesFrom(__DIR__.'/Routes/web.php');
 
         // Publishing is only necessary when using the CLI.
         //if ($this->app->runningInConsole()) {
